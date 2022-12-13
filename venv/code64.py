0f5d5ff6d3e001ef405e1704983384468fdab057
Claudio Melo <102559753+RaviensBR@users.noreply.github.com>
2022-12-13 19:28:51 -0300

    1

--- venv/code64.py
+++ venv/code64.py
@@ -1,8 +1,13 @@
 import base64
+#abrindo arquivo
+arquivo = open('C:\GR\GRPdv\GRPdvSync\GR.Snc.Client.Wpf.exe.config', 'r')
 
-Codigo_Base64 = input('cole aqui ')
-base64_bytes = Codigo_Base64.encode('ascii')
-message_bytes = base64.b64decode(base64_bytes)
-decodificado = message_bytes.decode('ascii')
+print(arquivo.read())
+# #decoder
+# Codigo_Base64 = arquivo
+# base64_bytes = Codigo_Base64.encode('ascii')
+# message_bytes = base64.b64decode(base64_bytes)
+# decodificado = message_bytes.decode('ascii')
 
-print(decodificado)
+# print(decodificado)
+# print(len(decodificado))cle
