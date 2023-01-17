08c4c9c35818c814d7fe20f98e17920a21c1814e
Claudio Melo <102559753+RaviensBR@users.noreply.github.com>
2023-01-16 22:28:23 -0300

    busca cep

--- venv/code64.py
+++ venv/code64.py
@@ -1,16 +1,34 @@
-import base64
-#abrindo arquivo
-arquivo = open('C:\GR\GRPdv\GRPdvSync\GR.Snc.Client.Wpf.exe.config', 'r')
-arquivo_open = arquivo
-print(arquivo.read())
-if arquivo_open
-#decoder
-# base64_message = ''
-# base64_bytes = base64_message.encode('ascii')
-# message_bytes = base64.b64decode(base64_bytes)
-# message = message_bytes.decode('ascii')
-
-# print(message)
-
-#print(decodificado)
-# print(len(decodificado))cle
+using System.Security.Cryptography;
+public static string Decrypt()
+        {
+            try
+            {
+                string textToDecrypt = "i8zIFROKod8rgyHv4MPHG5x46iH+tuJuEaaUSVJEdHs7SCIex1ih41v8kNrLcsMcQn+uSH2cdpmCDyVGx38MOBh/p7E18Xc8AxVaDmJNClTZS3Ug2LSng1PleKERrSaQr1x1sDTNHcrzKRKYyA6p4br39WrBUz08s2XlWOty8l7i1PrkqcV8Qj/Zqbh3SNGlJb03vTp0EZlmqxZtbKtlVJLTknkoOwHEsSyeVShMAWMMwmYIsGVsNX1SEaDJxvv3pMflPxiKD8gmtP43U1XB6KT74jPLfPDqHeRZycar08bY9GSpU2kmB6NaPQZxznLMzcVXwLr2kfZri21EfhN30qaHJ76U69qC+QBRHYJJwLfH5kQwgHZ7K5ohmI3+Ed0aIjTkNcqoUZDAs6ws4yIYrqMyN2SXbLINvz8PP9/98YKzo5NmIQ6La+7DQSuaQvomsuldh3tdr+4=";
+                
+                string ToReturn = "";
+                string publickey = "mZq3t6w9";
+                string secretkey = "2r5u8x/A";
+                byte[] privatekeyByte = { };
+                privatekeyByte = System.Text.Encoding.UTF8.GetBytes(secretkey);
+                byte[] publickeybyte = { };
+                publickeybyte = System.Text.Encoding.UTF8.GetBytes(publickey);
+                MemoryStream ms = null;
+                CryptoStream cs = null;
+                byte[] inputbyteArray = new byte[textToDecrypt.Replace(" ", "+").Length];
+                inputbyteArray = Convert.FromBase64String(textToDecrypt.Replace(" ", "+"));
+                using (DESCryptoServiceProvider des = new DESCryptoServiceProvider())
+                {
+                    ms = new MemoryStream();
+                    cs = new CryptoStream(ms, des.CreateDecryptor(publickeybyte, privatekeyByte), CryptoStreamMode.Write);
+                    cs.Write(inputbyteArray, 0, inputbyteArray.Length);
+                    cs.FlushFinalBlock();
+                    Encoding encoding = Encoding.UTF8;
+                    ToReturn = encoding.GetString(ms.ToArray());
+                }
+                return ToReturn;
+            }
+            catch (Exception ae)
+            {
+                throw new Exception(ae.Message, ae.InnerException);
+            }
+        }
