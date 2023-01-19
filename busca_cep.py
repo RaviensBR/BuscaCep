1eba4555af694304403b24fb4d1fa317e7c4959c
Claudio Melo <102559753+RaviensBR@users.noreply.github.com>
2023-01-19 19:16:16 -0300

    codigo pronto para busca

--- busca_cep.py
+++ busca_cep.py
@@ -1,12 +1,43 @@
 import requests
-cep = input('Digite seu Cep ' )
-link = f'https://viacep.com.br/ws/{cep}/json/'
-requisicao = requests.get(link)
-dados = requisicao.json()
-cep = dados['cep']
-logradouro = dados['logradouro']
-complemento = dados["complemento"]
-bairro = dados['bairro']
-localidade = dados['localidade']
-uf = dados['uf']
-print('Voce buscou pelo CEP',cep,'Com Bairro:',bairro, 'no Logradouro ',logradouro, complemento, uf )
+
+def buscaCep():
+    while True:
+        try:
+            cep = input('Digite os 8 numeros do CEP: ')
+            if cep.isnumeric() and len(cep) == 8:  # é um "número" de 8 dígitos
+                break  # sai do loop
+            print('O número deve ter 8 dígitos')
+        except ValueError:
+            print('Não foi digitado um número')
+
+    while True:
+        try:
+            link = f'https://viacep.com.br/ws/{cep}/json/'
+            requisicao = requests.get(link)
+            requisicao.raise_for_status()
+            dados = requisicao.json()
+            cep = dados['cep']
+            logradouro = dados['logradouro']
+            complemento = dados["complemento"]
+            bairro = dados['bairro']
+            localidade = dados['localidade']
+            uf = dados['uf']
+            print('Voce buscou pelo CEP', cep)
+            print('Endereço completo:', logradouro, complemento)
+            print('Bairro:', bairro)
+            print('Localidade:', localidade)
+            print('UF:', uf)
+            break
+        except requests.exceptions.HTTPError as errh:
+            print ("HTTP Error:",errh)
+        except requests.exceptions.ConnectionError as errc:
+            print ("Error Connecting:",errc)
+        except requests.exceptions.Timeout as errt:
+            print ("Timeout Error:",errt)
+        except KeyError:
+            print('CEP INVALIDO')
+        except requests.exceptions.RequestException as err:
+            print ("Something Else:",err)
+        cep = input('Digite os 8 numeros do CEP novamente: ')
+
+print(buscaCep())
