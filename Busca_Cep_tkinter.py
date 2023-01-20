47e02cc8b18327e33995cf1d35b683048997e429
Claudio Melo <102559753+RaviensBR@users.noreply.github.com>
2023-01-19 22:30:51 -0300

    limite de digitos

--- Busca_Cep_tkinter.py
+++ Busca_Cep_tkinter.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import requests
 
+
 def busca_cep(event=None):
     cep = entry.get()
     if len(cep) != 8 or not cep.isnumeric():
@@ -19,42 +20,48 @@ def busca_cep(event=None):
         localidade = dados.get("localidade", "")
         uf = dados.get("uf", "")
         cep = dados.get("cep", "")
-        
+
         cep_frame = tk.Frame(root, bd=1, relief="solid")
         cep_frame.place(x=30, y=80)
-        
+
         cep_box = tk.Label(cep_frame, text="CEP: "+cep, font=("Arial", 12))
         cep_box.pack()
-        
+
         endereco_frame = tk.Frame(root, bd=1, relief="solid")
         endereco_frame.place(x=30, y=110)
-        
-        endereco_box = tk.Label(endereco_frame, text="Endereço: "+logradouro+" "+complemento, font=("Arial", 12))
+
+        endereco_box = tk.Label(
+            endereco_frame, text="Endereço: "+logradouro+" "+complemento, font=("Arial", 12))
         endereco_box.pack()
-        
+
         bairro_frame = tk.Frame(root, bd=1, relief="solid")
         bairro_frame.place(x=30, y=140)
-        
-        bairro_box = tk.Label(bairro_frame, text="Bairro: "+bairro, font=("Arial", 12))
+
+        bairro_box = tk.Label(
+            bairro_frame, text="Bairro: "+bairro, font=("Arial", 12))
         bairro_box.pack()
-        
+
         cidade_frame = tk.Frame(root, bd=1, relief="solid")
         cidade_frame.place(x=30, y=170)
-        
-        cidade_box = tk.Label(cidade_frame, text="Cidade: "+localidade+"-"+uf, font=("Arial", 12))
+
+        cidade_box = tk.Label(cidade_frame, text="Cidade: " +
+                              localidade+"-"+uf, font=("Arial", 12))
         cidade_box.pack()
     except requests.exceptions.HTTPError as e:
         result_label.config(text=f"Erro: {e}")
 
+
 root = tk.Tk()
 root.geometry("400x300")
-root.title("Busca de CEP")
+root.title("Busca de CEP by:                               Claudio Melo")
 
-entry = tk.Entry(root, font=("Arial", 12))
+entry = tk.Entry(root, validate="key", validatecommand=(
+    root.register(lambda x: len(x) <= 8), '%P'))
 entry.place(x=30, y=30)
 entry.bind("<Return>", busca_cep)
 
-buscar_button = tk.Button(root, text="Buscar", font=("Arial", 12), command=busca_cep)
+buscar_button = tk.Button(
+    root, text="Buscar", font=("Arial", 12), command=busca_cep)
 buscar_button.place(x=320, y=28)
 
 result_label = tk.Label(root, font=("Arial", 12))
